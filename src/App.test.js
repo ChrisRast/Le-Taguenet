5fa03a4a92974194093082faf045a469ef4c6260
Christophe Rast <christophe@esl-education.org>
2018-05-07 14:44:22 +0200

    conf test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+	const div = document.createElement('div');
+
+	ReactDOM.render(
+		(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		),
+		div
+	);
 });
